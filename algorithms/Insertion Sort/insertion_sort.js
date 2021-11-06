47408273d1eee0f8e7f6e7384192cb3694e6f7b5
Eva Lam <evalam@hotmail.fr>
2021-11-06 15:38:56 +0100

    added insertion sort

--- algorithms/Insertion Sort/insertion_sort.js
+++ algorithms/Insertion Sort/insertion_sort.js
@@ -1,16 +1,28 @@
-const array = [21, 50, 44, 32, 67,23]
-function insertionSort(input){
-    let length = input.length;
-    for (let i = 1; i < length; i++) {
-        let key = input[i];
-        let j = i - 1;
-        while (j >= 0 && input[j] > key) {
-            input[j + 1] = input[j];
-            j = j - 1;
+function InsertionSort(inputArr){
+    //step 1: iterate on original unsorted array
+     for (i=0; i<inputArr.length; i++){ //O(n)
+      //step 2: keep track of current keyvalue 
+       let keyValue = inputArr[i]
+       let key = i 
+        //step 3: iterate on adjacent index
+        for (adjIndex = i+1; adjIndex<inputArr.length; adjIndex ++){
+          //step 4: compare its value with left subarray one by one 
+          while(inputArr[adjIndex]<inputArr[i]){
+            //Do a slide when the value of smaller than keyposition 
+            let temp = inputArr[i]; 
+            inputArr[i] = inputArr[adjIndex]; 
+            inputArr[adjIndex] = temp; 
+            //keep comparing with left elements til it is well sorted
+            adjIndex --;
+          }
+          keyValue = inputArr[adjIndex]; 
         }
-        input[j + 1] = key;
     }
-    return input;
-};
-
-console.log(insertionSort(array)); //-->[ 21, 23, 32, 44, 50, 67 ]
+    return inputArr
+  }
+  console.log(InsertionSort([45,65,12,23,90,2])) //--[ 2, 12, 23, 45, 65, 90 ]
+  console.log(InsertionSort([5,4,3,2,1,0]))//--[ 0, 1, 2, 3, 4, 5 ]
+  console.log(InsertionSort([]))//--[]
+  console.log(InsertionSort([90,34,21,4,233,12]))//--[ 4, 12, 21, 34, 90, 233 ]
+  console.log(InsertionSort([1,2,3,4,5]))//--[ 1, 2, 3, 4, 5 ]
+  console.log(InsertionSort([-1,2,-3,4,5]))//-- [-3, -1, 2, 4, 5 ]
